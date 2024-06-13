da929c195d64c31529cd213f88d020ac6f4a4bc9
Junhao Li <nicholasleeeman@gmail.com>
2024-06-13 11:49:28 +1000

    path alias

---
+++ my-react-website/src/components/foo/index.tsx
@@ -0,0 +1,5 @@
+import React, { JSX } from "react";    
+
+export const Foo: React.FC = (): JSX.Element => {
+    return <div>oh hai</div>;  
+}; 
--- my-react-website/vite.config.ts
+++ my-react-website/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Path from 'path'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      find: '@components',
+      replacement: Path.resolve(__dirname, 'src/components'),
+    },
+  },
 })
