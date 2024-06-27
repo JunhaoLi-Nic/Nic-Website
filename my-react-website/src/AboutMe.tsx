aab62f89a75eed3df08665049cd9ed7feb92e28a
Junhao Li <nicholasleeeman@gmail.com>
2024-06-27 14:33:58 +1000

    fixed the stureture of about me

--- my-react-website/src/AboutMe.tsx
+++ my-react-website/src/AboutMe.tsx
@@ -24,6 +24,7 @@ const AboutMe: React.FC = () => {
                     </p>
                 </div>
             </div>
+            <h1 className='calendar-header'>My GitHub Activity</h1>
             <div className='calendar'>
                 <EmbededGraph />
             </div>
