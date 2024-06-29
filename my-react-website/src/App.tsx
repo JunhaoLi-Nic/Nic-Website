d3b6d81d8909293be5b135f8904c19931fef1dc2
Junhao Li <nicholasleeeman@gmail.com>
2024-06-29 11:48:41 +1000

    refactor contact form to compoent

--- my-react-website/src/App.tsx
+++ my-react-website/src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from '@components/Header/header'
 import MainPage from '@components/main_page.tsx';
 import AboutMe from './AboutMe.tsx'
 import Project from './Project.tsx'
+import Contact from '@components/contact.tsx'
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -11,7 +12,6 @@ const MainLayout: React.FC = () => {
     <div className="app">
       <Header />
       <MainPage />
-     
       <div className="testimonial-5">
         <div className="row-4">
           <span className="my-4">
@@ -56,88 +56,7 @@ const MainLayout: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="contact-form">
-        <div className="column-2">
-          <div className="component-1">
-            <div className="frame-1000015865">
-              <span className="your-name">
-                Your name
-              </span>
-            </div>
-          </div>
-          <div className="component-2">
-            <div className="frame-10000158651">
-              <span className="your-name-1">
-                Email
-              </span>
-            </div>
-          </div>
-          <div className="component-3">
-            <div className="frame-10000158652">
-              <span className="your-name-2">
-                Your website (If exists)
-              </span>
-            </div>
-          </div>
-          <div className="component-4">
-            <div className="frame-10000158653">
-              <span className="how-can-ihelp">
-                How can I help?
-              </span>
-            </div>
-          </div>
-          <div className="button">
-            <div className="button-3">
-              <span className="resume">
-                Get In Touch
-              </span>
-            </div>
-            <div className="button-4">
-              <img className="vector-70" src="src/assets/vectors/Vector20_x2.svg" />
-            </div>
-            <div className="button-5">
-              <img className="subtract" src="src/assets/vectors/Subtract5_x2.svg" />
-            </div>
-            <div className="button-6">
-              <img className="vector-71" src="src/assets/vectors/Vector45_x2.svg" />
-            </div>
-            <div className="button-7">
-              <img className="group-1" src="src/assets/vectors/Group11_x2.svg" />
-            </div>
-          </div>
-        </div>
-        <div className="column-3">
-          <div className="frame-1000015867">
-            <div className="frame-1000015874">
-              <div className="frame-1000015866">
-                <span className="lets">
-                  Let’s
-                </span>
-                <span className="talk">
-                  talk
-                </span>
-                <span className="for">
-                  for
-                </span>
-              </div>
-              <span className="something-special">
-                Something special
-              </span>
-            </div>
-            <span className="iseek-to-push-the-limits-of-creativity-to-create-high-engaging-user-friendly-and-memorable-interactive-experiences">
-              I seek to push the limits of creativity to create high-engaging, user-friendly, and memorable interactive experiences.
-            </span>
-          </div>
-          <div className="frame-1000015868">
-            <div className="youremailgmail-com">
-              Youremail@gmail.com
-            </div>
-            <span className="container-3">
-              1234567890
-            </span>
-          </div>
-        </div>
-      </div>
+      <Contact />
       <div className="footer">
         <div className="frame-16">
           <div className="logo-11">
---
+++ my-react-website/src/components/contact.tsx
@@ -0,0 +1,90 @@
+import "@css/contact.css";
+import React from "react";
+
+const Contact: React.FC = () => {
+    return (
+        <div className="contact-form">
+        <div className="column-2">
+          <div className="component-1">
+            <div className="frame-1000015865">
+              <span className="your-name">
+                Your name
+              </span>
+            </div>
+          </div>
+          <div className="component-2">
+            <div className="frame-10000158651">
+              <span className="your-name-1">
+                Email
+              </span>
+            </div>
+          </div>
+          <div className="component-3">
+            <div className="frame-10000158652">
+              <span className="your-name-2">
+                Your website (If exists)
+              </span>
+            </div>
+          </div>
+          <div className="component-4">
+            <div className="frame-10000158653">
+              <span className="how-can-ihelp">
+                How can I help?
+              </span>
+            </div>
+          </div>
+          <div className="button">
+            <div className="button-3">
+              <span className="resume">
+                Get In Touch
+              </span>
+            </div>
+            <div className="button-4">
+              <img className="vector-70" src="src/assets/vectors/Vector20_x2.svg" />
+            </div>
+            <div className="button-5">
+              <img className="subtract" src="src/assets/vectors/Subtract5_x2.svg" />
+            </div>
+            <div className="button-6">
+              <img className="vector-71" src="src/assets/vectors/Vector45_x2.svg" />
+            </div>
+            <div className="button-7">
+              <img className="group-1" src="src/assets/vectors/Group11_x2.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="column-3">
+          <div className="frame-1000015867">
+            <div className="frame-1000015874">
+              <div className="frame-1000015866">
+                <span className="lets">
+                  Let’s
+                </span>
+                <span className="talk">
+                  talk
+                </span>
+                <span className="for">
+                  for
+                </span>
+              </div>
+              <span className="something-special">
+                Something special
+              </span>
+            </div>
+            <span className="iseek-to-push-the-limits-of-creativity-to-create-high-engaging-user-friendly-and-memorable-interactive-experiences">
+              I seek to push the limits of creativity to create high-engaging, user-friendly, and memorable interactive experiences.
+            </span>
+          </div>
+          <div className="frame-1000015868">
+            <div className="youremailgmail-com">
+              Youremail@gmail.com
+            </div>
+            <span className="container-3">
+              1234567890
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+}
+export default Contact;
