05e062133d12b22478b049984a9280c7726f01b9
Junhao Li <nicholasleeeman@gmail.com>
2024-06-30 12:53:25 +1000

    Adjusted footer

--- my-react-website/src/components/footer.tsx
+++ my-react-website/src/components/footer.tsx
@@ -1,23 +1,23 @@
 import "@css/footer.css";
 import React from "react";
+import { Link } from 'react-router-dom';
+import {Donut} from "@components/spline/grass-donuts";
 
 const Footer: React.FC = () => {
     return (
         <div className="footer">
         <div className="frame-16">
-          <div className="logo-11">
-            <img className="group-27" src="src/assets/vectors/Group4_x2.svg" />
-          </div>
           <div className="personal-2">
-            Personal
+            <Donut />
           </div>
+          <Link to ="/" className="personal-1">Nic.</Link>
         </div>
         <div className="frame-1000015869">
           <div className="personal">
             @ 2019-2024 
           </div>
           <span className="made-in-figma">
-            Made In Figma
+            Made by Nicholas Li
           </span>
         </div>
       </div>
