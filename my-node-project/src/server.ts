e6cab458724e4181b79c4c4d8e4bdde988aac5a3
Junhao Li <nicholasleeeman@gmail.com>
2024-07-22 17:26:50 +1000

    added testing ymal, need to implment Jest later

--- my-node-project/src/server.ts
+++ my-node-project/src/server.ts
@@ -29,23 +29,23 @@ const transporter = nodemailer.createTransport({
 
 // Define the email sending route
 app.post('/send-email', async (req: Request, res: Response) => {
-    const { name, email, message, website } = req.body;
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: 'nicholasleeeman@gmail.com',
-      subject: `Message from ${name}`,
-      text: `You have received a new message from ${name} they are website is  ${website} (${email}): ${message}`
-    };
-  
-    try {
-      await transporter.sendMail(mailOptions);
-      res.status(200).send('Email sent successfully');
-    } catch (error) {
-      console.error('Failed to send the email', error);
-      res.status(500).send('Failed to send email');
-    }
-  });
-  
+  const { name, email, message, website } = req.body;
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'nicholasleeeman@gmail.com',
+    subject: `Message from ${name}`,
+    text: `You have received a new message from ${name} they are website is  ${website} (${email}): ${message}`
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error('Failed to send the email', error);
+    res.status(500).send('Failed to send email');
+  }
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
