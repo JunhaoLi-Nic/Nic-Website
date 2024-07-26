c780bf73edf721df7c2659ee6edf0d776ef4f765
Junhao Li <nicholasleeeman@gmail.com>
2024-07-26 13:28:27 +1000

    refactor button out of header

--- my-react-website/src/components/Header/header.tsx
+++ my-react-website/src/components/Header/header.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '@css/header.css';
-import {Donut} from '@components/spline/grass-donuts';
 import { HashLink } from 'react-router-hash-link';
-
+import '@css/header.css';
+import { Donut } from '@components/spline/grass-donuts';
+import HamburgerButton from '@components/Tool/HamburgerButton';
 
 const Header: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div className="header">
@@ -14,15 +18,15 @@ const Header: React.FC = () => {
                 <div className="logo-1">
                     <Donut/>
                 </div>
-                <Link to ="/" className="personal-1">Nic.</Link>
+                <Link to="/" className="personal-1">Nic.</Link>
             </div>
-            <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>☰</button>
+            <HamburgerButton onClick={toggleMenu} />
             <div className={`taskbaar ${isOpen ? 'open' : ''}`}>
                 <Link to="/aboutme" className="link">About Me</Link>
                 <Link to="/Resume" className="link">Resume</Link>
                 <Link to="/projects" className="link">Project</Link>
                 <HashLink to="/#contact" className="link">Contact Me</HashLink>
-                <div className="button-8 inside-menu"> {/* New class to style differently if needed */}
+                <div className="button-8 inside-menu">
                     <span className="resume-1">Resume</span>
                     <div className="download">
                         <img className="icon" src="src/assets/vectors/Icon24_x2.svg" alt="Download" />
