df1de780f6eeb0cc5f17d5acfd9edba25e94ff52
Nicholas <nicholasleeeman@gmail.com>
2024-08-04 11:51:05 +1000

    added import to Project

--- my-react-website/src/Project.tsx
+++ my-react-website/src/Project.tsx
@@ -1,27 +1,30 @@
 import './Project.css'; // Ensure this CSS file is correctly linked and loaded
 import Header from '@components/Header/header';
 import Footer from '@components/footer';
+import AMSLImage from './assets/images/AMSL_airplane.jpg';
+import SuperMarioImage from './assets/images/SuperMario.jpg';
+import ARImage from './assets/images/AR_Glasses.jpg';
 
 const projects = [
   {
     id: '01  (Industrial Project)',
     title: ' Utilization of Large Language Models (LLMs) in Aerospace Software Testing',
     description: 'Developed a prototype application integrating advanced LLM to automate the generation of test scenarios from aerospace software requirements. This project aims to improve testing efficiency and accuracy in compliance with safety and regulatory standards like DO-178C.',
-    imageUrl: 'src/images/AMSL_airplane.jpg',
+    imageUrl:{AMSLImage} ,
     readMoreLink: 'https://github.com/JunhaoLi-Nic'
   },
   {
     id: '02 (Object oriented programming)',
     title: 'Super Mario Game Simulator',
     description: ' A Mario-inspired roguelike game was developed, adhering to SOLID design principles for robustness and maintainability. The project involved detailed planning using UML and sequential diagrams, followed by the creation of a game engine that allowed for smooth control of the Mario character. Various game scenes and functionalities were also designed and implemented to enhance gameplay and optimize performance. This development showcased advanced skills in game design and software engineering, focusing on delivering an engaging and well-organized gaming experience.',
-    imageUrl: 'src/images/SuperMario.jpg',
+    imageUrl: {SuperMarioImage},
     readMoreLink: 'https://github.com/JunhaoLi-Nic/Mario-Game-2D'
   },
   {
     id: '03 (Augmented Reality Glass)',
     title: 'AR Shopping List glasses',
     description: 'It is built on Unity with integration from Android Studio and the XReal (Nreal) SDK, enables AR Shopping List organization by detecting objects dynamically. We designed a user friendly interface that lets users engage with a virtual shopping list shown on AR glasses. The system uses a CSV file to efficiently store and update the shopping list, which users can manage through a simple UI in the AR setting. Key features include real time item detection and validation, supported by the app\'s ability to identify items from the shopping list in the store\'s environment and confirm selections directly through the glasses.',
-    imageUrl: 'src/images/AR_Glasses.jpg',
+    imageUrl: {ARImage},
     readMoreLink: 'https://github.com/YapSengKuang/AR-Glasses'
   }
 ];
