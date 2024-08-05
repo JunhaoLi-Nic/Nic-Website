b3ac6c32a1d466359c154dcc7a8d549fd2be6a81
Nicholas <nicholasleeeman@gmail.com>
2024-08-05 18:08:44 +1000

    fixed the phone version issues

--- my-react-website/src/components/footer.tsx
+++ my-react-website/src/components/footer.tsx
@@ -1,26 +1,26 @@
 import "@css/footer.css";
 import React from "react";
 import { Link } from 'react-router-dom';
-import {Donut} from "@components/spline/grass-donuts";
+import { Donut } from "@components/spline/grass-donuts";
 
 const Footer: React.FC = () => {
-    return (
-        <div className="footer">
-        <div className="frame-16">
-          <div className="personal-2">
-            <Donut />
-          </div>
-          <Link to ="/" className="nic">Nic.</Link>
+  return (
+    <div className="footer">
+      <div className="frame-16">
+        <div className="personal-2">
+          <Donut />
         </div>
-        <div className="frame-1000015869">
-          <div className="personal">
-            @ 2019-2024 
-          </div>
-          <span className="made-in-figma">
-            Made by Nicholas Li
-          </span>
-        </div>
+        <Link to="/" className="nic">Nic.</Link>
       </div>
-    );
+      <div className="frame-1000015869">
+        <div className="personal">
+          @ 2019-2024
+        </div>
+        <span className="made-in-figma">
+          Made by Nicholas Li
+        </span>
+      </div>
+    </div>
+  );
 }
 export default Footer;
